e249ee7b2b39811ce39f94fc6daa1a6939228679
Ashraf Sellak <sellakachraf9@gmail.com>
2025-10-19 20:53:02 +0100

    add Navigation Component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import logo from '../assets/logo.png'
+const Navigation = () => {
+    return (
+       <nav>
+        <div className='logo'>
+            <img src={logo} alt="Do some coding logo"/>
+        </div>
+
+        <ul>
+            <li href="#">Home</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Navigation;
+
